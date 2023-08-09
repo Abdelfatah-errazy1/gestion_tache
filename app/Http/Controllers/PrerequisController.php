59289ebaa76caf8064a00e789df94a06059dcf73
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-08-09 22:07:00 +0100

    fix the controller of prerequis

--- app/Http/Controllers/PrerequisController.php
+++ app/Http/Controllers/PrerequisController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prerequis;
 use Illuminate\Http\Request;
 
 class PrerequisController extends Controller
 {
-    //
+    public function store(Request $request)  {
+        $validate=$request->validate([
+            'prerequis'=>'required|string|max:150',
+            'tache'=>'required|exists:taches,id',
+        ]);
+        
+        $model=Prerequis::create($validate);
+        // dd($validate);
+        return redirect(route('taches.edit',$model->tache));
+    }
+    public function update(Request $request,$id)  {
+        $validate=$request->validate([
+            'prerequis'=>'required|string|max:150',
+            'tache'=>'required|exists:taches,id',
+        ]);
+        $model=Prerequis::find($id);
+        $model->update($validate);
+        redirect(route('taches.edit',$model->tache));;
+    }
+    public function delete($id)  {
+        $model=Prerequis::find($id);
+        $model->delete();
+        redirect(route('taches.edit',$model->tache));
+    }
+
+
 }
