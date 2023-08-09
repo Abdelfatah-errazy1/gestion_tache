728ac0695f148c6c139050ca59eed30143040060
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-08-09 22:04:48 +0100

    fixing the controller of conraint

--- app/Http/Controllers/ContraintController.php
+++ app/Http/Controllers/ContraintController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contraint;
 use Illuminate\Http\Request;
 
 class ContraintController extends Controller
 {
-    //
+    
+    public function store(Request $request)  {
+        $validate=$request->validate([
+            'contraint'=>'required|string|max:150',
+            'tache'=>'required|exists:taches,id',
+        ]);
+        
+        $model=Contraint::create($validate);
+        // dd($validate);
+        return redirect(route('taches.edit',$model->tache));
+    }
+    public function update(Request $request,$id)  {
+        $validate=$request->validate([
+            'contraint'=>'required|string|max:150',
+            'tache'=>'required|exists:taches,id',
+        ]);
+        $model=Contraint::find($id);
+        $model->update($validate);
+        redirect(route('taches.edit',$model->tache));;
+    }
+    public function delete($id)  {
+        $model=Contraint::find($id);
+        $model->delete();
+        redirect(route('taches.edit',$model->tache));
+    }
+
 }
