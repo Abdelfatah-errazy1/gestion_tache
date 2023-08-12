5dae6d3f15302625e4bbd225d0220da45886b060
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-08-12 13:15:26 +0100

    fixing the show view

--- resources/views/taches/show.blade.php
+++ resources/views/taches/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.layouts')
 @section('content')
-  <section style="background-color: #eee;">
+  <section style="background-color: #fff;">
     <div class="container py-5">
       <nav aria-label="breadcrumb">
           <ol class="breadcrumb">
@@ -8,49 +8,67 @@
             <li class="breadcrumb-item active" aria-current="page">Edit tache</li>
           </ol>
       </nav>
-      <form action="{{ route('taches.update',$model->id) }}" class="row" method="POST">
+      <form action="{{ route('taches.update',$model->id) }}" class="row p-4" method="POST">
         @method('put')
         @csrf
         <div class="mb-3">
           <label  class="form-label">Titre</label>
-          <input type="text" class="form-control" value="{{ $model->titre }}" name="titre" placeholder="titre....">
+          <input type="text" class="form-control" value="{{ $model->titre }}" readonly name="titre" placeholder="titre....">
         </div>
         <div class="mb-3 col col-md-6">
           <label  class="form-label">Priorite</label>
-          <select class="form-control" name="priorite" >
-            <option value="1">tres urgent</option>
-            <option value="2">urgent</option>
-            <option value="3">moyen</option>
-            <option value="4">canceled</option>
-            <option value="5">stopped</option>
+          <select class="form-control"readonly name="priorite" >
+            @switch($model->priorite)
+            @case(1)
+              <option class="badge bg-danger "> Tres urgenent</option>
+              @break
+            @case(2)
+              <option class="badge bg-warning ">  urgenent</option>
+              @break
+            @case(3)
+              <option class="badge bg-success "> moyen</option>
+              @break        
+            @default
+            <option class="badge bg-success "> moyen</option>
+          @endswitch
           </select>
         </div>
         <div class="mb-3 col col-md-6">
           <label  class="form-label">Statut</label>
-          <select class="form-control" name="statut" >
-            <option value="1">En cours</option>
-            <option value="2">Rapporter</option>
-            <option value="3">Terminner</option>
+          <select class="form-control" readonly name="statut" >
+            @switch($model->statut)
+            @case(1)
+              <option class="badge bg-warning-subtle text-dark "> En cours</option>
+              @break
+            @case(2)
+              <option class="badge bg-warning ">  A reporter </option>
+              @break
+            @case(3)
+              <option class="badge bg-success "> Terminer</option>
+              @break        
+            @default
+            <option class="badge bg-success "> moyen</option>
+          @endswitch
           </select>
         </div>
         <div class="mb-3 col col-md-4">
           <label  class="form-label">Date Debut</label>
-          <input type="date" class="form-control" value="{{ $model->date_debut }}" name="date_debut" placeholder="date_debut....">
+          <input type="date" class="form-control" readonly value="{{ $model->date_debut }}" name="date_debut" placeholder="date_debut....">
         </div>
         <div class="mb-3 col col-md-4">
           <label  class="form-label">date fin</label>
-          <input type="date" class="form-control" value="{{ $model->date_fin }}" name="date_fin" placeholder="date_fin....">
+          <input type="date" class="form-control" readonly value="{{ $model->date_fin }}" name="date_fin" placeholder="date_fin....">
         </div>
         <div class="mb-3 col col-md-4">
           <label  class="form-label">Date Effective</label>
-          <input type="date" class="form-control" name="date_effective" value="{{ $model->date_effective }}" placeholder="date_effective....">
+          <input type="date" class="form-control" readonly name="date_effective" value="{{ $model->date_effective }}" placeholder="date_effective....">
         </div>
         <div class="mb-3">
           <label  class="form-label">Description</label>
-          <textarea class="form-control" name="description" rows="3">{{ $model->description }}</textarea>
+          <textarea class="form-control" readonly name="description" rows="3">{{ $model->description }}</textarea>
         </div>
         <div class="mb-3">
-          <button class="btn btn-primary" type="submit" >Modifier </button>
+          <a href="{{ route('taches.index') }}" class="btn btn-primary"   >Home</a>
         </div>
 
       </form>
