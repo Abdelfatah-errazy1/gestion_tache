b50031f1836f69a595cf7e3f78e8c70edb53851a
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-09-15 19:57:04 +0100

    fixing the layout

---
+++ resources/views/components/_navSide.blade.php
@@ -0,0 +1,51 @@
+<aside id="sidebar" class="sidebar">
+
+  <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li><!-- End Dashboard Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="components-alerts.html">
+            <i class="bi bi-circle"></i><span>Alerts</span>
+          </a>
+        </li>
+        <li>
+          <a href="components-accordion.html">
+            <i class="bi bi-circle"></i><span>Accordion</span>
+          </a>
+        </li>
+      </ul>
+    </li><!-- End Components Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link " data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-people-fill"></i><span>Clients</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="forms-nav" class="nav-content collapse show" data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="" class="active">
+            <i class="bi bi-circle"></i><span>Liste Clients</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i class="bi bi-circle"></i><span>Ajouter</span>
+          </a>
+        </li>
+        
+      </ul>
+    </li><!-- End Forms Nav -->
+
+  </ul>
+
+</aside>
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,30 +1,74 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-    <title>Taches </title>
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    {{-- bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <title>Forms / Elements - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  </head>
-  <body>
-    
+  <!-- Favicons -->
+  <link href='{{ asset('assets/imgs/logo.png') }}' rel="icon">
+  <link href='{{ asset("assets/img/apple-touch-icon.png") }}' rel="apple-touch-icon">
 
-    @include('components.header')
-    <div class="container my-3">
-      @yield('content')
-    </div>
-    @include('components.footer')
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href='{{ asset("assets/vendor/bootstrap/css/bootstrap.min.css") }}' rel="stylesheet">
+  <link href='{{ asset("assets/vendor/bootstrap-icons/bootstrap-icons.css") }}' rel="stylesheet">
+  <link href='{{ asset("assets/vendor/boxicons/css/boxicons.min.css") }}' rel="stylesheet">
+  <link href='{{ asset("assets/vendor/quill/quill.snow.css") }}' rel="stylesheet">
+  <link href='{{ asset("assets/vendor/quill/quill.bubble.css") }}' rel="stylesheet">
+  <link href='{{ asset("assets/vendor/remixicon/remixicon.css") }}' rel="stylesheet">
+  <link href='{{ asset("assets/vendor/simple-datatables/style.css") }}' rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <!-- Template Main CSS File -->
+  <link href='{{ asset("assets/css/style.css") }}' rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/css/print.css') }}">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Aug 30 2023 with Bootstrap v5.3.1
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  @include('components._header')
+  <!-- ======= Sidebar ======= -->
+  @include('components._navSide')
+  <!-- End Sidebar-->
+  @yield('content')
+
+  <!-- ======= Footer ======= -->
+  @include('components._footer')
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src='{{ asset("assets/vendor/apexcharts/apexcharts.min.js") }}'></script>
+  <script src='{{ asset("assets/vendor/bootstrap/js/bootstrap.bundle.min.js") }}'></script>
+  <script src='{{ asset("assets/vendor/chart.js/chart.umd.js") }}'></script>
+  <script src='{{ asset("assets/vendor/echarts/echarts.min.js") }}'></script>
+  <script src='{{ asset("assets/vendor/quill/quill.min.js") }}'></script>
+  <script src='{{ asset("assets/vendor/simple-datatables/simple-datatables.js") }}'></script>
+  <script src='{{ asset("assets/vendor/tinymce/tinymce.min.js") }}'></script>
+  <script src='{{ asset("assets/vendor/php-email-form/validate.js") }}'></script>
+
+  <!-- Template Main JS File -->
+  <script src='{{ asset("assets/js/main.js") }}'></script>
+
+
     @if(session('success'))
       <script>
           // SweetAlert alert for successful task saving
@@ -36,12 +80,7 @@
               timer: 1500
           });
       </script>
-    @endif
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
-    <!-- Include Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
+    @endif <script>
 
       function showTab(tabId) {
         // Hide all tab contents
